4155a3513aa3333bf4494ac0f408554f01895240
Refik ONAL <refik.onal@metglobal.com>
2016-01-11 13:49:37 +0200

    Initial Commit

---
+++ Snips/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Snips
+//
+//  Created by ytu-refik-onal on 1/11/16.
+//  Copyright © 2016 ytu-refik-onal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Snips/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Snips
+//
+//  Created by ytu-refik-onal on 1/11/16.
+//  Copyright © 2016 ytu-refik-onal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
